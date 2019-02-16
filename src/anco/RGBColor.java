5b3a3cb46901602d21b8d66139734214a5b5bc2e
kavezo <jwang19@stanford.edu>
2019-02-15 23:48:04 -0800

    Fixed hex to RGB.

--- src/anco/RGBColor.java
+++ src/anco/RGBColor.java
@@ -1,6 +1,9 @@
 package anco;
 
 public class RGBColor extends Color {
+	public static final RGBColor RED = new RGBColor(255, 0, 0);
+	public static final RGBColor GREEN = new RGBColor(0, 255, 0);
+	public static final RGBColor BLUE = new RGBColor(0, 0, 255);
 	
 	public RGBColor(int r, int g, int b) {
 		super(3);
@@ -11,9 +14,9 @@ public class RGBColor extends Color {
 		super(3);
 		this.color = new float[this.n];
 		
-		this.color[0] = rgb >> 4;
-		this.color[1] = (rgb >> 2) & 3;
-		this.color[2] = rgb & 3;
+		this.color[0] = rgb >> 16;
+		this.color[1] = (rgb >> 8) & 255;
+		this.color[2] = rgb & 255;
 	}
 
 	@Override
@@ -29,7 +32,14 @@ public class RGBColor extends Color {
 	@Override
 	public ColorDisplay display() {
 		ColorDisplay cd = new ColorDisplay(this.n);
+		float total = this.color[0] + this.color[1] + this.color[2];
+		cd.ratios = new float[]{this.color[0]/total, this.color[1]/total, this.color[2]/total};
+		cd.names = new String[]{"Red", "Green", "Blue"};
+		cd.colors = new RGBColor[]{RED, GREEN, BLUE};
 		return cd;
 	}
-
+	
+	public String toString() {
+		return "[R: " + this.color[0] + ", G: " + this.color[1] + ", B: " + this.color[2] + "]";
+	}
 }
